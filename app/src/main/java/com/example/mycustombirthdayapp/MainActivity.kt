42c363fbfc3ff9ca3ea2cc64f746d88060abac15
Chroniustain <93010262+Chroniustain@users.noreply.github.com>
2021-11-16 18:17:05 +0200

    "Update layout xml." "Make it more readable. Add some features."

--- app/src/main/java/com/example/mycustombirthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/mycustombirthdayapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mycustombirthdayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mycustombirthdayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
