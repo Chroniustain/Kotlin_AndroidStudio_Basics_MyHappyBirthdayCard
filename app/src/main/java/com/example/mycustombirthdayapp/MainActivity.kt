16b47932c5bb424e3e9026b3923ab52f1a73b027
Chroniustain <93010262+Chroniustain@users.noreply.github.com>
2021-11-16 20:31:25 +0200

    "Add reset button. Add code for functional buttons."

--- app/src/main/java/com/example/mycustombirthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/mycustombirthdayapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.mycustombirthdayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.widget.ToggleButton
 import com.example.mycustombirthdayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +12,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.youButton.setOnClickListener {
+            Toast.makeText(this, "SURPRISE!!!", Toast.LENGTH_SHORT).show()
+            binding.sisterImage.setImageResource(R.drawable.funny_female_dog)
+        }
+
+        binding.meButton.setOnClickListener {
+            Toast.makeText(this, "SURPRISE!!!", Toast.LENGTH_SHORT).show()
+            binding.myImage.setImageResource(R.drawable.funny_male_dog)
+        }
+
+        binding.resetCurtainButton.setOnClickListener{
+            Toast.makeText(this, "THE END.", Toast.LENGTH_SHORT).show()
+            binding.sisterImage.setImageResource(R.drawable.theatre_curtains)
+            binding.myImage.setImageResource(R.drawable.theatre_curtains)
+        }
     }
 }
